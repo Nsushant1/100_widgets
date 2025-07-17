c1c7632a8010543aef58a0552ac6bbc7666aa807
Sushant Neupane <nsushaant72@gmail.com>
2025-07-17 20:14:45 +0545

    listed 100 widgets

---
+++ android/app/src/main/kotlin/com/example/u_100_widgets/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.u_100_widgets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/100_widgets/alert_dialog.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Widget01 extends StatelessWidget {
+  const Widget01({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
